2ccd492910836e2db4902a2a54df13e4b2a4f26d
Izabela Janusz <afrodyzjak100@gmail.com>
2025-10-04 22:37:17 +0200

    calculateResult interaction

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import Form from "./Form";
 import Container from "./Container";
 import Result from "./Result";
 
 function App() {
+
+const [result, setResult] = useState({});
+
+const calculateResult = (currency, amount) => {
+  const rate = currencies
+  .find(({short}) => short === currency)
+  .rate;
+};
+
   return (
     <Container>
       <Form />
       <Result />
+      calculateResult={calculateResult}
     </Container>
+    
   );
 }
 
