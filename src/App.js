588f13ea3d66eb328c77efa89be03a61c597ceea
Izabela Janusz <afrodyzjak100@gmail.com>
2025-10-07 19:53:22 +0200

    code alignment

--- src/App.js
+++ src/App.js
@@ -5,33 +5,24 @@ import Result from "./Result";
 import currencies from "./currencies";
 
 function App() {
+  const [result, setResult] = useState({});
 
-const [result, setResult] = useState({});
+  const calculateResult = (currency, amount) => {
+    const rate = currencies.find(({ short }) => short === currency).rate;
 
-const calculateResult = (currency, amount) => {
-  const rate = currencies
-  .find(({short}) => short === currency)
-  .rate;
-
-  setResult ({
-  sourceAmount: +amount,
-  targetAmount: amount/rate,
-  currency,
-});
-
-}
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
 
   return (
     <Container>
-      <Form 
-      calculateResult={calculateResult}
-      result={result}
-      />
+      <Form calculateResult={calculateResult} result={result} />
       <Result />
-      
     </Container>
-    
   );
-};
+}
 
 export default App;
