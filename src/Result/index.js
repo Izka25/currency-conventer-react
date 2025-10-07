31ca6bbbe178c3f549bf86dd40c11f42d2184570
Izabela Janusz <afrodyzjak100@gmail.com>
2025-10-07 19:48:36 +0200

    result interaction

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,8 +1,16 @@
 import "./style.css";
 
-const Result = () => (
+const Result = ({ result }) => (
   <p className="label">
-    Wychodzi:<strong className="result">N/A</strong>
+    {result !== undefined && (
+      <>
+        {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
+        <strong>
+          {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+        </strong>
+      </>
+    )}
+    <Result result={result} />
   </p>
 );
 
