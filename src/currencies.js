547b94db584c86031b8eb4d6589386da3c33ab2a
Izabela Janusz <afrodyzjak100@gmail.com>
2025-10-08 20:41:09 +0200

    alignment

--- src/currencies.js
+++ src/currencies.js
@@ -1,54 +1,54 @@
 const currencies = [
-    {
-        short: "EUR",
-        name: "Euro",
-        rate: 4.56,
-    },
-    {
-      short: "AUD",
-      name: "Dolar australijski",
-      rate: 2.87,
-    },
-    {
-        short: "GBP",
-        name: "Funt brytyjski",
-        rate: 5.38,
-    },
-    {
-        short: "JPY",
-        name: "Jen japoński",
-        rate: 3.45,
-    },
-    {
-        short: "CHF",
-        name: "Frank szwajcarski",
-        rate: 4.30,  
-    },
-    {
-        short: "USD",
-        name: "Dolar amerykański",
-        rate: 3.97,
-    },
-    {
-        short: "HKD",
-        name: "Dolar hongkongu",
-        rate: 0.51,
-    },
-    {
-        short: "CAD",
-        name: "Dolar kanadyjski",
-        rate: 3.12,
-    },
-    {
-        short: "NZD",
-        name: "Dolar Nowozelandzki",
-        rate: 2.64,
-    },
-    {
-        short: "SGD",
-        name: "Dolar Singapurski",
-        rate: 2.95,
-    },
+  {
+    short: "EUR",
+    name: "Euro",
+    rate: 4.56,
+  },
+  {
+    short: "AUD",
+    name: "Dolar australijski",
+    rate: 2.87,
+  },
+  {
+    short: "GBP",
+    name: "Funt brytyjski",
+    rate: 5.38,
+  },
+  {
+    short: "JPY",
+    name: "Jen japoński",
+    rate: 3.45,
+  },
+  {
+    short: "CHF",
+    name: "Frank szwajcarski",
+    rate: 4.3,
+  },
+  {
+    short: "USD",
+    name: "Dolar amerykański",
+    rate: 3.97,
+  },
+  {
+    short: "HKD",
+    name: "Dolar hongkongu",
+    rate: 0.51,
+  },
+  {
+    short: "CAD",
+    name: "Dolar kanadyjski",
+    rate: 3.12,
+  },
+  {
+    short: "NZD",
+    name: "Dolar Nowozelandzki",
+    rate: 2.64,
+  },
+  {
+    short: "SGD",
+    name: "Dolar Singapurski",
+    rate: 2.95,
+  },
 ];
 
 export default currencies;
