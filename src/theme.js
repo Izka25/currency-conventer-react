4ed9e9c0440d0eee561c411c076752d94b77745c
Izabela Janusz <afrodyzjak100@gmail.com>
2025-11-16 22:42:51 +0100

    adding a theme.js file and adding in index.js: theme and ThemeProvider

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme ={
+    color:{
+      codGray: "#141212",
+      blueRibbon: "#0062FF",
+      dodgerBlue: "#2BD1FF",
+      yellow: "#FFFF00",
+      pastelGreen: "#90EE90",
+      emperor: "#555555",
+    }
+}
