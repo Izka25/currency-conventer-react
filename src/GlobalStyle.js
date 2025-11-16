83ce505871391aa1fa4ff1ddfddc84940706ac07
Izabela Janusz <afrodyzjak100@gmail.com>
2025-11-16 23:30:56 +0100

    adding a GlobalStyle file and in index.js GlobalStyle, adding background.jpg and adding a background

---
+++ src/GlobalStyle.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./background.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*,
+::after,
+::before {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: "Montserrat", sans-serif;
+  background-image: url("${background}");
+  background-repeat: no-repeat;
+  background-size: cover;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-position: center;
+}
+`
