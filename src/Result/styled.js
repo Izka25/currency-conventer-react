6115544193df30c0556a2b15b5dc3cff223895f0
Izabela Janusz <afrodyzjak100@gmail.com>
2025-11-20 06:16:54 +0100

    moving Result to Form and removing it from the main folder

--- src/Result/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Label = styled.label`
-    font-size: 30px;
-    font-style: italic;
-    font-weight: bold;
-    padding: 10px;
-    text-align: center;
-`;
